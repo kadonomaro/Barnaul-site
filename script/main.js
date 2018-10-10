5a76f422d7ff4353ff3ef0d35bddc1d427636d98
kadonomaro <denis.afer@gmail.com>
2018-10-11 00:10:39 +0700

    upgrade bad js with "for"

--- script/main.js
+++ script/main.js
@@ -10,25 +10,41 @@ var showElemLisavenko = document.querySelector('#lisavenko');
 var elemGebler = document.querySelector('#people-gebler');
 var showElemGebler = document.querySelector('#gebler');
 
+var elements = document.querySelectorAll('.human-article');
 
 
 
 elemPolzunov.onclick = function(evt) {
   evt.preventDefault();
+  for (i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('content-show');
+}
   showElemPolzunov.classList.toggle('content-show');
+  
 };
 
 elemDemidov.onclick = function(evt) {
   evt.preventDefault();
+  for (i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('content-show');
+}
   showElemDemidov.classList.toggle('content-show');
+  
 };
-
 elemLisavenko.onclick = function(evt) {
   evt.preventDefault();
+  for (i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('content-show');
+}
   showElemLisavenko.classList.toggle('content-show');
+  
 };
-
 elemGebler.onclick = function(evt) {
   evt.preventDefault();
+  for (i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('content-show');
+}
   showElemGebler.classList.toggle('content-show');
+  
 };
+
