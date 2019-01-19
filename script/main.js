11b80910bbcecc2129cb23e44f66d7a8145d0765
kadonomaro <denis.afer@gmail.com>
2019-01-19 22:50:50 +0700

    added fade in animation

--- script/main.js
+++ script/main.js
@@ -1,33 +1,43 @@
 //смена статей об известных людях
-var thumbs = document.querySelectorAll('.human-card__link');
-var articles = document.querySelectorAll('.human-article');
 
-for (let i = 0; i < thumbs.length; i++) {
-    thumbs[i].addEventListener('click', function (evt) {
-        evt.preventDefault();
-        articles.forEach(article => {
-            article.classList.remove('content-show');
-        });
-        articles[i].classList.add('content-show');
-    });
-    
-}
+document.addEventListener("DOMContentLoaded", function () {
+  
+  var thumbs = document.querySelectorAll('.human-card__link');
+  var articles = document.querySelectorAll('.human-article');
 
-//закрытие меню
-var closeMenu = document.querySelector('.nav__item-close');
-var nav = document.querySelector('.nav');
+  for (let i = 0; i < thumbs.length; i++) {
+      thumbs[i].addEventListener('click', function (evt) {
+          evt.preventDefault();
+          articles.forEach(article => {
+              article.classList.remove('content-show');
+          });
+          articles[i].classList.add('content-show');
+      });
+  }
 
-closeMenu.addEventListener('click', isClose);
+  //закрытие меню
+  var closeMenu = document.querySelector('.nav__item-close');
+  var nav = document.querySelector('.nav');
 
-function isClose(){
-  nav.classList.toggle('is-close');
-};
+  closeMenu.addEventListener('click', isClose);
 
-
-var mainBG = document.querySelector('.main__background');
-var mainTitle = document.querySelector('.main__title');
-
-window.onscroll = function () {
-  mainBG.style.filter = "blur(5px)";
-  mainTitle.style.opacity = "1";
-}
+  function isClose(){
+    nav.classList.toggle('is-close');
+  };
+  //размытие главного экрана и появление заголовка
+  var mainBG = document.querySelector('.main__background');
+  var mainTitle = document.querySelector('.main__title');
+  var historyPhoto = document.querySelectorAll('.history__photo');
+  
+  //добавление эффектов про скролле
+  window.addEventListener('scroll', function () {
+    mainBG.style.filter = "blur(5px)";
+    mainTitle.style.opacity = "1";
+    if (window.pageYOffset >= (historyPhoto[0].offsetTop - 500)) {
+      historyPhoto.forEach(photo => {
+        photo.classList.remove('history__photo_fade-left');
+        photo.classList.remove('history__photo_fade-right');
+      });
+    }
+  })
+});
