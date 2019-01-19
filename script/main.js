44b05367d7927264d407bd2624f890c5505d5180
kadonomaro <denis.afer@gmail.com>
2019-01-19 18:00:05 +0700

    Optimized JS code with keep of functionality

--- script/main.js
+++ script/main.js
@@ -1,52 +1,17 @@
-var elemPolzunov = document.querySelector('#people-polzunov');
-var showElemPolzunov = document.querySelector('#polzunov');
+//смена статей об известных людях
+var thumbs = document.querySelectorAll('.human-card__link');
+var articles = document.querySelectorAll('.human-article');
 
-var elemDemidov = document.querySelector('#people-demidov');
-var showElemDemidov = document.querySelector('#demidov');
-
-var elemLisavenko = document.querySelector('#people-lisavenko');
-var showElemLisavenko = document.querySelector('#lisavenko');
-
-var elemGebler = document.querySelector('#people-gebler');
-var showElemGebler = document.querySelector('#gebler');
-
-var elements = document.querySelectorAll('.human-article');
-
-// var cssHref = document.querySelector('.css-href');
-// cssHref.href = 'css/light-style.css';
-
-
-
-elemPolzunov.onclick = function(evt) {
-  evt.preventDefault();
-  for (i = 0; i < elements.length; i++) {
-    elements[i].classList.remove('content-show');
+for (let i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener('click', function (evt) {
+        evt.preventDefault();
+        articles.forEach(article => {
+            article.classList.remove('content-show');
+        });
+        articles[i].classList.add('content-show');
+    });
+    
 }
-  showElemPolzunov.classList.toggle('content-show');
-};
-
-elemDemidov.onclick = function(evt) {
-  evt.preventDefault();
-  for (i = 0; i < elements.length; i++) {
-    elements[i].classList.remove('content-show');
-}
-  showElemDemidov.classList.toggle('content-show');
-};
-elemLisavenko.onclick = function(evt) {
-  evt.preventDefault();
-  for (i = 0; i < elements.length; i++) {
-    elements[i].classList.remove('content-show');
-}
-  showElemLisavenko.classList.toggle('content-show');
-};
-elemGebler.onclick = function(evt) {
-  evt.preventDefault();
-  for (i = 0; i < elements.length; i++) {
-    elements[i].classList.remove('content-show');
-}
-  showElemGebler.classList.toggle('content-show');
-};
-
 
 //закрытие меню
 var closeMenu = document.querySelector('.nav__item-close');
@@ -57,8 +22,6 @@ closeMenu.addEventListener('click', isClose);
 function isClose(){
   nav.classList.toggle('is-close');
 };
-
-
 
 
 var mainBG = document.querySelector('.main__background');
